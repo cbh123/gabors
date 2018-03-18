6fd4e10fd5ec5ad1f29dea7c661450f4323b9473
Charlie Holtz <charlieholtz@gmail.com>
2018-03-18 15:36:40 -0400

    Updated Filters

--- gabor_final.py
+++ gabor_final.py
@@ -3,11 +3,21 @@ from scipy import arange, sqrt, array, amax, ceil, io, fliplr, flipud
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import ipdb
+import time
+from tqdm import tqdm
 
 
-rot = np.array([90])
-RF_siz = sp.arange(7,40,2)
+# rot = np.array([90,-67.5,-45,-22.5,0,45,67.5])
+# RF_siz = sp.arange(33,67,2)
+# Div = sp.arange(4,3.15,-.05)
+
+rot = np.array([90,-67.5,-45,-22.5,0,22.5,45,67.5])
+RF_siz = sp.arange(33,67,2)
 Div = sp.arange(4,3.15,-.05)
+
+
+
 
 def create_gabor(rot,RF_siz,Div,plot):
     count = 0
@@ -18,31 +28,35 @@ def create_gabor(rot,RF_siz,Div,plot):
     G      = 0.3
     phases = [0, np.pi/2]
 
-    # Initialize Filerbank
-    alt_fb = np.zeros((39,39,1,34),dtype=np.float32)
+    # Initialize Filterbank
+    alt_fb = np.zeros((65,65,1,272),dtype=np.float32)
 
-    for k in range(0,numFilterSizes):
-        for r in range(0,numSimpleFilters):
+
+    # for k in tqdm(range(0,numFilterSizes-1)):
+    for k in tqdm(range(1,numFilterSizes+1)):
+        for r in range(1,numSimpleFilters+1):
+            # time.sleep()
 
             f = np.zeros([RF_siz[numFilterSizes-1],RF_siz[numFilterSizes-1]])
             fx = np.zeros([RF_siz[numFilterSizes-1],RF_siz[numFilterSizes-1]])
 
             ## Parameters
-            theta     = rot[r]*(np.pi/180)
-            filtSize  = RF_siz[k]
+            theta     = rot[r-1]*(np.pi/180)
+            filtSize  = RF_siz[k-1]
 
-            img_center = ceil(20.0) ## New center for padding with zeros
+            img_center = ceil(33.0) ## New center for padding with zeros
 
-            center    = ceil(filtSize/2)+1 ## Old and possibly more accurate center
+            center    = ceil(filtSize/2.0) ## Old and possibly more accurate center
 
             filtSizeL = center-1
             filtSizeR = filtSize-filtSizeL-1
-            sigmaq    = (sigma[k]) * (sigma[k])
+            sigmaq    = (sigma[k-1]) * (sigma[k-1])
+
 
             # Compute filter values
-            for iPhi in range(0,2):
-                for i in range( int((-1 * filtSizeL)),int(filtSizeR+1)):
-                    for j in range( int((-1 * filtSizeL)),int(filtSizeR+1)):
+            for iPhi in range(1,3):
+                for i in range(int(-1 * filtSizeL),int(filtSizeR+1)):
+                    for j in range(int(-1 * filtSizeL),int(filtSizeR+1)):
 
                         if (sqrt((i**2)+(j**2))>(filtSize/2 )) :
                             E = 0
@@ -50,7 +64,7 @@ def create_gabor(rot,RF_siz,Div,plot):
                             x = i*np.cos(theta) - j*np.sin(theta)
                             y = i*np.sin(theta) + j*np.cos(theta)
 
-                            E = np.exp((-1*((x**2)+ (G**2) * (y**2)))/(2*sigmaq))*np.cos(2*np.pi*x/lamb[k] + phases[iPhi])
+                            E = np.exp((-1*((x**2)+ (G**2) * (y**2)))/(2*sigmaq))*np.cos(2*np.pi*x/lamb[k-1] + phases[iPhi-1])
 
                         f[int(j+img_center-1),int(i+img_center-1)] = E
 
@@ -64,10 +78,13 @@ def create_gabor(rot,RF_siz,Div,plot):
                 count += 1
 
                 if (plot):
-                    plt.imshow(x_image,cmap='Greys')
+
+                    plt.imshow(f,cmap='Greys')
                     plt.show()
 
+    import ipdb; ipdb.set_trace()
     return (np.array(alt_fb))
+
 
 
 
@@ -78,14 +95,14 @@ def create_gabor(rot,RF_siz,Div,plot):
 
 ## Create Dictionary ---------------
 gabor_array = create_gabor(rot,RF_siz,Div,False)
-bias = np.zeros([34])
+bias = np.zeros([21])
 gabor_dictionary = {}
 gabor_dictionary['gabors'] = gabor_array,bias
 
 f,x = gabor_dictionary['gabors']
 
 ## Save Dictionary ----------------------
-np.save('/Users/charlieholtz/Desktop/hermann/Gabor Filters/gabor_dictionary.npy', gabor_dictionary)
+np.save('/Users/charlieholtz/Desktop/gabors/gabor_dictionary.npy', gabor_dictionary)
 
 ## Test Dictionary
 #weights = tf.get_variable('weights', shape = [filter_height, filter_width, input_channels/groups, num_filters])
